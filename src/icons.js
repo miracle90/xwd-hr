57019aea3ca1cd64c3b1cb3a2cf64e36c38beb22
李亚运(11112733) <11112733@vivo.xyz>
2021-06-01 17:54:01 +0800

    antd icon按需加载

--- src/icons.js
+++ src/icons.js
@@ -45,3 +45,51 @@ export {
 export {
   default as QuestionCircleOutline
 } from '@ant-design/icons/lib/outline/QuestionCircleOutline'
+
+export {
+  default as CaretDownOutline
+} from '@ant-design/icons/lib/outline/CaretDownOutline'
+
+export {
+  default as CaretRightOutline
+} from '@ant-design/icons/lib/outline/CaretRightOutline'
+
+export {
+  default as SwitcherOutline
+} from '@ant-design/icons/lib/outline/SwitcherOutline'
+
+export {
+  default as CloseCircleOutline
+} from '@ant-design/icons/lib/outline/CloseCircleOutline'
+
+export {
+  default as CheckCircleOutline
+} from '@ant-design/icons/lib/outline/CheckCircleOutline'
+
+export {
+  default as ExclamationCircleOutline
+} from '@ant-design/icons/lib/outline/ExclamationCircleOutline'
+
+export {
+  default as CaretDownFill
+} from '@ant-design/icons/lib/fill/CaretDownFill'
+
+export {
+  default as CaretRightFill
+} from '@ant-design/icons/lib/fill/CaretRightFill'
+
+export {
+  default as SwitcherFill
+} from '@ant-design/icons/lib/fill/SwitcherFill'
+
+export {
+  default as CloseCircleFill
+} from '@ant-design/icons/lib/fill/CloseCircleFill'
+
+export {
+  default as CheckCircleFill
+} from '@ant-design/icons/lib/fill/CheckCircleFill'
+
+export {
+  default as ExclamationCircleFill
+} from '@ant-design/icons/lib/fill/ExclamationCircleFill'
